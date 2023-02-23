dd601c38628521b78b04e07ec590ac208c42e555
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-23 18:20:38 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from "lodash.throttle";
 
+const feedbackParent = document.querySelector('.feedback-form');
+const feedbackEmail = document.querySelector('input');
+const feedbackMessage = document.querySelector('textarea');
+const KEY_USER_EMAIL = "email";
+const KEY_USER_MESSAGE = "message";
+
+const updateOutput = () => {
+    feedbackEmail.value = localStorage.getItem(KEY_USER_EMAIL) || "";
+    feedbackMessage.value = localStorage.getItem(KEY_USER_MESSAGE) || "";
+} 
+
+window.addEventListener("load", updateOutput);
+
+const saveFormValues = () => {
+    localStorage.setItem(KEY_USER_EMAIL, feedbackEmail.value);
+    localStorage.setItem(KEY_USER_MESSAGE, feedbackMessage.value)
+}
+
+
+feedbackParent.addEventListener("input", throttle(saveFormValues, 500));
+
+feedbackParent.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    localStorage.clear();
+    feedbackEmail.value = feedbackMessage.value = "";
+})
