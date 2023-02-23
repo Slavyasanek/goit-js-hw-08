2c6d2d8ff701b531fc96eedc4ad550c1c3d23127
Slavyasanek <117304306+Slavyasanek@users.noreply.github.com>
2023-02-23 17:32:21 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+const KEY_TO_LOCAL_STORAGE = "videoplayer-current-time";
+
+player.on('timeupdate', throttle((data) => {
+    localStorage.setItem(KEY_TO_LOCAL_STORAGE, data.seconds);
+}, 1000));
+
+player.setCurrentTime(localStorage.getItem(KEY_TO_LOCAL_STORAGE));
